efbb877a57007dd8b15f72a3cf24d17c8d1d1585
Vinícius Augusto <vinicius.a.silva60@aluno.senai.br>
2026-09-24 17:02:28 -0300

    Feat: Rename of the files from the extension .html

--- Pages/dashboard.php
+++ Pages/dashboard.php
@@ -9,67 +9,7 @@
 
 <body>
 
-    <!-- SIDEBAR -->
-<aside class="sidebar">
-
-    <div class="logo">
-
-        <!-- LOGO DA EMPRESA -->
-        <div class="logo-image">
-            <img src="./imagens/logo-prisma.png" alt="Logo da empresa">
-        </div>
-
-
-    </div>
-
-    <nav class="
-menu">
-
-        <a href="./dashboard.html" class="menu-item active">
-            <i class="fa-solid fa-house"></i>
-            <span>Dashboard</span>
-        </a>
-
-        <a href="./estoque.html" class="menu-item">
-            <i class="fa-solid fa-box"></i>
-            <span>Estoque</span>
-        </a>
-
-        <a href="./" class="menu-item">
-            <i class="fa-solid fa-dollar-sign"></i>
-            <span>Financeiro</span>
-        </a>
-
-        <a href="./configuracoes.html" class="menu-item">
-            <i class="fa-solid fa-gear"></i>
-            <span>Configurações</span>
-        </a>
-
-        <a href="#" class="menu-item">
-            <i class="fa-solid fa-store"></i>
-            <span>Ver Loja</span>
-        </a>
-
-    </nav>
-
-    <div class="sidebar-bottom">
-
-        <div class="user">
-            <div class="user-icon">
-                <i class="fa-solid fa-user"></i>
-            </div>
-
-            <div>
-                <strong>Administrador</strong>
-                <small>Sistema de Gestão</small>
-            </div>
-        </div>
-
-        <i class="fa-solid fa-right-from-bracket logout"></i>
-
-    </div>
-
-</aside>
+<?php require_once "sidebar.php" ?>
 
 
     <!-- CONTEÚDO PRINCIPAL -->
